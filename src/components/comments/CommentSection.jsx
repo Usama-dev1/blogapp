ef6ac49cbf4dc47e47aeb6fad2fd646ab97a62cf
raja.usamah <raja.usamah@gmail.com>
2026-07-05 01:59:47 +0500

    added icons and like button

--- src/components/comments/CommentSection.jsx
+++ src/components/comments/CommentSection.jsx
@@ -3,7 +3,7 @@ import CommentInputCard from "./CommentInputCard";
 
 const CommentSection = () => {
   return (
-    <div className="body-width border-t border-border flex items-center p-8">
+    <div className="bg-secondary body-width border-t border-border flex items-center p-8">
       <div className="w-full py-8 flex flex-col justify-center gap-4 items-center">
         <CommentCard />
         <CommentInputCard />
--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import { AiOutlineLike } from "react-icons/ai";
+import { CiShare1 } from "react-icons/ci";
+import { FaComment } from "react-icons/fa";
+import { GoDotFill } from "react-icons/go";
 
 const PostDetails = () => {
   return (
     <div className="body-width">
       <h1 className="heading-main p-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, sit?
+        Lorem ipsum do0lor sit amet consectetur adipisicing elit. Dolores, sit?
       </h1>
-      <p className="prose-text p-4">
+      <div className="w-full px-6 flex flex-wrap items-center justify-between border-t border-b border-border py-4 gap-4">
+        <div className="flex flex-wrap items-center gap-4 text-sm">
+          <div className="flex items-center gap-2">
+            <p className="font-semibold text-body-text text-base">
+              Post By <span className="text-brand">Usama</span>
+            </p>
+          </div>
+
+          <div className="flex items-center gap-1 text-muted-text">
+            <span className="opacity-50 text-xl">
+              <GoDotFill />
+            </span>
+            <p>10 mins ago</p>
+          </div>
+        </div>
+        <button className="flex items-center gap-2 btn-md btn-icon">
+          <span className="text-md">Comment on this Post</span>
+          <span className="text-md">
+            <FaComment />
+          </span>
+        </button>
+        <button className="flex items-center gap-2 btn-md btn-icon">
+          <span className="text-md">Share this Post</span>
+          <span className="text-md">
+            <CiShare1 />
+          </span>
+        </button>
+
+        <button className="flex items-center gap-2 btn-md btn-icon">
+          <span className="text-md">Like this Post</span>
+          <span className="text-md">
+            <AiOutlineLike />
+          </span>
+        </button>
+      </div>
+      <p className="prose-text p-4 font-article">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod
         adipisci qui, modi maiores iure nesciunt laudantium in odio ipsum amet,
         officia cupiditate numquam sunt ullam quibusdam unde ut. Modi saepe fuga
