24ea5c70c00b1975786adbeae2a2db9fac5e7e42
raja.usamah <raja.usamah@gmail.com>
2026-07-15 02:43:06 +0500

    fixed scroll issue

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/common/Navbar";
 import { Outlet } from "react-router";
 const MainLayout = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col">
+    <div className="w-full min-h-screen overflow-y-scroll flex flex-col">
       <Navbar />
       <main className="flex-1 flex flex-col items-center py-3">
         <Outlet />
